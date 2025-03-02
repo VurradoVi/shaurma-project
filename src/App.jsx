77c96cf869da8a2fd5d4ca04e7b77678a0ae8eab
Vlad <02kvg03@mail.ru>
2025-03-02 18:36:57 +0300

    api shaurma

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,18 @@ import Container from "./components/Container";
 import Header from "./components/Header";
 import ShaurmaBlock from "./components/ShaurmaBlock";
 import shaurmaItems from "./assets/shaurma.json";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://67c46ab7c4649b9551b38dbe.mockapi.io/items")
+      .then((res) => setData(res.data));
+  }, []);
+
   return (
     <div className="mx-6">
       <Container>
@@ -13,7 +23,7 @@ function App() {
         <div className="mt-9">
           <h2 className="font-extrabold text-3xl mb-6">Вся Шаурма</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-4">
-            {shaurmaItems.map((obj) => (
+            {data.map((obj) => (
               <ShaurmaBlock key={obj.id} {...obj} />
             ))}
           </div>
